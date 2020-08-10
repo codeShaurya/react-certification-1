94dc46dfcd1e484401ce91c8e3182f6ae566dd24
Shubham Maurya <code.shaurya@gmail.com>
2020-08-10 22:11:20 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -7,41 +7,45 @@ import {applyPolyfills, defineCustomElements} from 'h8k-components/loader';
 
 const ARTICLES = [
   {
+    title: "A message to our customers",
+    upvotes: 12,
+    date: "2020-01-24",
+  },
+  {
     title: "Alphabet earnings",
     upvotes: 22,
-    date: "2011-11-23",
+    date: "2019-11-23",
   },
   {
     title: "Artificial Mountains",
-    upvotes: 200,
-    date: "2019-11-23",
+    upvotes: 2,
+    date: "2019-11-22",
   },
   {
     title: "Scaling to 100k Users",
     upvotes: 72,
-    date: "2019-10-21",
-  },
-  {
-    title: "A message to our customers",
-    upvotes: 12,
-    date: "2019-10-22",
+    date: "2019-01-21",
   },
   {
     title: "the Emu War",
     upvotes: 24,
-    date: "2018-04-01",
+    date: "2019-10-21",
   },
   {
     title: "What's SAP",
     upvotes: 1,
-    date: "2017-01-21",
+    date: "2019-11-21",
   },
   {
     title: "Simple text editor has 15k monthly users",
-    upvotes: 83,
-    date: "2020-02-22",
+    upvotes: 7,
+    date: "2010-12-31",
   },
 ];
+
+ARTICLES.sort(function (a, b) {
+  return b.upvotes - a.upvotes;
+});
 
 ReactDOM.render(<App articles={ARTICLES} />, document.getElementById('root'));
 registerServiceWorker();
