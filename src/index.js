7575fc2ab05005c0eae5afae4ad86a453eb4e6e0
Shubham Maurya <code.shaurya@gmail.com>
2020-08-10 22:08:48 +0530

    Done

--- src/index.js
+++ src/index.js
@@ -7,39 +7,39 @@ import {applyPolyfills, defineCustomElements} from 'h8k-components/loader';
 
 const ARTICLES = [
   {
-    title: "A message to our customers",
-    upvotes: 12,
-    date: "2020-01-24",
-  },
-  {
     title: "Alphabet earnings",
     upvotes: 22,
-    date: "2019-11-23",
+    date: "2011-11-23",
   },
   {
     title: "Artificial Mountains",
-    upvotes: 2,
-    date: "2019-11-22",
+    upvotes: 200,
+    date: "2019-11-23",
   },
   {
     title: "Scaling to 100k Users",
     upvotes: 72,
-    date: "2019-01-21",
+    date: "2019-10-21",
+  },
+  {
+    title: "A message to our customers",
+    upvotes: 12,
+    date: "2019-10-22",
   },
   {
     title: "the Emu War",
     upvotes: 24,
-    date: "2019-10-21",
+    date: "2018-04-01",
   },
   {
     title: "What's SAP",
     upvotes: 1,
-    date: "2019-11-21",
+    date: "2017-01-21",
   },
   {
     title: "Simple text editor has 15k monthly users",
-    upvotes: 7,
-    date: "2010-12-31",
+    upvotes: 83,
+    date: "2020-02-22",
   },
 ];
 
